bf85b0497b5b67670a16878995951b52f76a107b
Kacper Kowalski <repcak95@gmail.com>
2018-01-17 17:06:06 +0100

    Finished app

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -11,6 +11,11 @@ import App from './components/App';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
+import Raven from 'raven-js';
+import {sentry_url} from "./data/config";
+
+Raven.config(sentry_url).install();
+
 const router = (
   <Provider store={store}>
     <Router history={history}>
